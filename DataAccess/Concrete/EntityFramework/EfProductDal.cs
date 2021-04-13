0787dc79926e99212908ec2a5539a6bf97fb47bd
İbrahim KARADAĞ <56786042+KodcuKrdg@users.noreply.github.com>
2021-04-13 17:49:44 +0300

    EntityFramework Öğrenildi.

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,27 +14,53 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            //IDısposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext()) // işlem bitince bellekten silinir
+            {
+                var addedEntity = context.Entry(entity); //"Entry()"-> Git veritabanındaki nesneye ilişkilendir demek 
+                addedEntity.State = EntityState.Added; // "Entry()" ile eşitleneni "Ekleme" "Güncelleme" "Silme" yapar
+                context.SaveChanges(); //"SaveChanges()"-> işlemleri gerçekleştir
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            //IDısposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext()) // işlem bitince bellekten silinir
+            {
+                var deletedEntity = context.Entry(entity); //"Entry()"-> Git veritabanındaki nesneye ilişkilendir demek 
+                deletedEntity.State = EntityState.Deleted; // "Entry()" ile eşitleneni "Ekleme" "Güncelleme" "Silme" yapar
+                context.SaveChanges(); //"SaveChanges()"-> işlemleri gerçekleştir
+            }
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
         }
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //filter == null ? "null ise" : "nul değilse"
+                //context.Set<Product>().ToList() Veritabanındaki "Product" tablosunu liste olarak verir 
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
         }
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            //IDısposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext()) // işlem bitince bellekten silinir
+            {
+                var updatedEntity = context.Entry(entity); //"Entry()"-> Git veritabanındaki nesneye ilişkilendir demek 
+                updatedEntity.State = EntityState.Modified; // "Entry()" ile eşitleneni "Ekleme" "Güncelleme" "Silme" yapar
+                context.SaveChanges(); //"SaveChanges()"-> işlemleri gerçekleştir
+            }
         }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını Bağlamak
+    //DbContext :  Microsoft.EntityFrameworkCore ile geldi
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // Hangi Veritabanı ile ilişkindirilecek onu seçiyoruz
+        {
+            //Trusted_Connection=true kullanıcı adı paralo istemeden
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; } // "DbSet<Product>"-> hangi Class "Products"-> hangi tabloya karşılık geliyor
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
